9aba87b2e078f17f59e467a451e69c72e1b96df7
gokul_krishna <gokulkrishnagk62@gmail.com>
2024-01-13 16:47:46 -0500

    fixed updateCart API issue by adding a getCartItem method before updating the cart

--- helpers/eComShopManagerHelper.js
+++ helpers/eComShopManagerHelper.js
@@ -22,47 +22,20 @@ exports.addProducts = function (req) {
 }
 
 
-exports.updateCartMapper = function (req) {
+exports.updateCartMapper = function (req, response) {
     let cartMapper = {}
 
+    // check if the item in the request body exists in the cart table
+    let itemExists = response && response.length > 0 && response[0].quantity > 0
+    let updateQuantity;
+    if (itemExists) {
+        updateQuantity = req.body.addToCart ? (req.body.quantity || 0) + 1 : req.body.quantity - 1
+    } else {
+        updateQuantity = req.body.addToCart ? req.body.quantity || 0 : 0
+    }
+
     cartMapper.pid = req.body.pid
-    cartMapper.quantity = req.body.addToCart ? (req.body.quantity || 0) + 1 : req.body.quantity - 1
+    cartMapper.quantity = updateQuantity
 
     return cartMapper
 }
-
-// exports.updateTaskMapper = function (req) {
-//     let updateTaskMapper = {}
-//
-//     if (utils.isNotNull(req.query.task_label)) updateTaskMapper.task_label = req.query.task_label
-//     if (utils.isNotNull(req.query.task_description)) updateTaskMapper.task_description = req.query.task_description
-//     if (utils.isNotNull(req.query.status)) updateTaskMapper.status = req.query.status
-//     updateTaskMapper.modified_date = getDateTime(new Date())
-//
-//     return updateTaskMapper
-// }
-//
-// function getDateTime(today) {
-//
-//     let year = today.getFullYear();
-//     let month = today.getMonth() + 1; // getMonth() returns 0-11
-//     let day = today.getDate();
-//
-//     let hours = today.getHours();
-//     let minutes = today.getMinutes();
-//     let seconds = today.getSeconds();
-//
-//     // Pad single digits with a leading zero
-//     month = month < 10 ? '0' + month : month;
-//     day = day < 10 ? '0' + day : day;
-//     hours = hours < 10 ? '0' + hours : hours;
-//     minutes = minutes < 10 ? '0' + minutes : minutes;
-//     seconds = seconds < 10 ? '0' + seconds : seconds;
-//
-//     let dateStr = year + '-' + month + '-' + day;
-//     let timeStr = hours + ':' + minutes + ':' + seconds;
-//
-//     return dateStr + ' ' + timeStr
-// }
-//
-// exports.getDateTime = getDateTime
