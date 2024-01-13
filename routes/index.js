1fa744c6169dbd2d1d9f83dcb7f50e1e8e7d1df9
gokul_krishna <gokulkrishnagk62@gmail.com>
2024-01-13 17:54:46 -0500

    added fs module to package.json

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const eComShop = require('../controllers/eComShopController')
 exports.addProducts = function (req, res) {
     eComShop.addProducts(req, (err, response) => {
         if (err) {
-            console.log(err)
             res.status(err.code ? err.code : 500).send(err)
         } else {
             res.status(201).send(response)
