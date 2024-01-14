ce20aeb217d828192b5f91b5ca93f6296b4677f7
gokul_krishna <gokulkrishnagk62@gmail.com>
2024-01-13 19:37:27 -0500

    tne

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ try {
 // Set up storage engine
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, uploadDir)
+        cb(null, '../uploads/')
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
