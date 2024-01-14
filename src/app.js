3e2d2462b5f112a5b7cec6bcfecc0a79b5a0f878
gokul_krishna <gokulkrishnagk62@gmail.com>
2024-01-13 19:49:34 -0500

    fixed the issue for postProducts failing on server

--- src/app.js
+++ src/app.js
@@ -68,7 +68,7 @@ try {
 // Set up storage engine
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '../uploads/')
+        cb(null, uploadDir)
     },
     filename: function (req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
